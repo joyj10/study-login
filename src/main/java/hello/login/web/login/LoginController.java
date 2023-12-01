5593908cca1f678439873cdf59911626172baba8
dev_one <micool1030@gmail.com>
2023-12-01 12:28:58 +0900

    [feat] 세션 관리 직접 개발

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -35,7 +35,7 @@ public class LoginController {
         Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
         if (loginMember == null) {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
-            return "login/loginForm";
+            return "login/loginForm"; 
         }
 
         // 로그인 성공 처리
